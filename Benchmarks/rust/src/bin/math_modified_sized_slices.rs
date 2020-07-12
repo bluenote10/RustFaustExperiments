b5f754a4249cb6a9dd753ae99b0b16a0742e10e0
Fabian Keller <github.100.fkeller@spamgourmet.com>
2020-07-12 10:16:24 +0200

    added rust generated sources

---
+++ Benchmarks/rust/src/bin/math_modified_sized_slices.rs
@@ -0,0 +1,160 @@
+#![allow(unused_parens)]
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_mut)]
+#![allow(non_upper_case_globals)]
+
+extern crate libm;
+
+use faust_benchmarks::benchmark_runner::run_benchmark;
+use faust_benchmarks::types::{FaustDsp, Meta, ParamIndex, UI};
+
+// Generated intrinsics:
+
+// Generated class:
+
+pub struct Dsp {
+    fSampleRate: i32,
+}
+
+impl FaustDsp for Dsp {
+    type T = f32;
+
+    fn new() -> Dsp {
+        Dsp { fSampleRate: 0 }
+    }
+    fn metadata(&self, m: &mut dyn Meta) {
+        m.declare("filename", "math.dsp");
+        m.declare("name", "math");
+    }
+
+    fn get_sample_rate(&self) -> i32 {
+        return self.fSampleRate;
+    }
+    fn get_num_inputs(&self) -> i32 {
+        return 8;
+    }
+    fn get_num_outputs(&self) -> i32 {
+        return 1;
+    }
+    fn get_input_rate(&self, channel: i32) -> i32 {
+        let mut rate: i32;
+        match (channel) {
+            0 => {
+                rate = 1;
+            }
+            1 => {
+                rate = 1;
+            }
+            2 => {
+                rate = 1;
+            }
+            3 => {
+                rate = 1;
+            }
+            4 => {
+                rate = 1;
+            }
+            5 => {
+                rate = 1;
+            }
+            6 => {
+                rate = 1;
+            }
+            7 => {
+                rate = 1;
+            }
+            _ => {
+                rate = -1;
+            }
+        }
+        return rate;
+    }
+    fn get_output_rate(&self, channel: i32) -> i32 {
+        let mut rate: i32;
+        match (channel) {
+            0 => {
+                rate = 1;
+            }
+            _ => {
+                rate = -1;
+            }
+        }
+        return rate;
+    }
+
+    fn class_init(sample_rate: i32) {}
+    fn instance_reset_params(&mut self) {}
+    fn instance_clear(&mut self) {}
+    fn instance_constants(&mut self, sample_rate: i32) {
+        self.fSampleRate = sample_rate;
+    }
+    fn instance_init(&mut self, sample_rate: i32) {
+        self.instance_constants(sample_rate);
+        self.instance_reset_params();
+        self.instance_clear();
+    }
+    fn init(&mut self, sample_rate: i32) {
+        Dsp::class_init(sample_rate);
+        self.instance_init(sample_rate);
+    }
+
+    fn build_user_interface(&self, ui_interface: &mut dyn UI<Self::T>) {
+        Self::build_user_interface_static(ui_interface);
+    }
+
+    fn build_user_interface_static(ui_interface: &mut dyn UI<Self::T>) {
+        ui_interface.open_vertical_box("math");
+        ui_interface.close_box();
+    }
+
+    fn get_param(&self, param: ParamIndex) -> Option<Self::T> {
+        match param.0 {
+            _ => None,
+        }
+    }
+
+    fn set_param(&mut self, param: ParamIndex, value: Self::T) {
+        match param.0 {
+            _ => {}
+        }
+    }
+
+    fn compute(&mut self, count: i32, inputs: &[&[Self::T]], outputs: &mut [&mut [Self::T]]) {
+        let input0 = &inputs[0][..count as usize];
+        let input1 = &inputs[1][..count as usize];
+        let input2 = &inputs[2][..count as usize];
+        let input3 = &inputs[3][..count as usize];
+        let input4 = &inputs[4][..count as usize];
+        let input5 = &inputs[5][..count as usize];
+        let input6 = &inputs[6][..count as usize];
+        let input7 = &inputs[7][..count as usize];
+        let output0 = &mut outputs[0][..count as usize];
+
+        for i in 0..count {
+            output0[i as usize] = (((((input2[i as usize] as f32)
+                + (input3[i as usize] as f32))
+                * ((input0[i as usize] as f32) + (input1[i as usize] as f32)))
+                / (((input6[i as usize] as f32) + (input7[i as usize] as f32))
+                    * ((input4[i as usize] as f32) + (input5[i as usize] as f32))))
+                as f32);
+        }
+    }
+}
+
+const SAMPLE_RATE: i32 = 44100;
+
+fn main() {
+    println!("Size of DSP struct: {}", std::mem::size_of::<Dsp>());
+
+    run_benchmark(
+        || {
+            let mut dsp = Dsp::new();
+            dsp.init(SAMPLE_RATE);
+            Box::new(dsp)
+        },
+        SAMPLE_RATE,
+    );
+}
